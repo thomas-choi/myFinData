f9f9e44cefa15bb870cc5edabc6d4b9246309070
Thomas Choi <thomas.choi@neuralmatrixllc.com>
2023-05-02 00:43:08 -0700

    pull container from docker.hub, remove gpu

--- Ops/docker_jobs.sh
+++ Ops/docker_jobs.sh
@@ -7,6 +7,7 @@ cd $ProjDIR
 mycmd="$CMDPATH/apply_gmodel.sh"
 echo "Excute $mycmd @ `pwd`"
 
-docker run --gpus all --rm --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --shm-size=2g -u 1000:1000 \
+docker run -it --rm --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --shm-size=2g -u 1000:1000 \
     -v `pwd`:`pwd` -w `pwd` -v /etc/timezone:/etc/timezone -v /etc/localtime:/etc/localtime -v /etc/passwd:/etc/passwd \
-    -v /etc/group:/etc/group -v /tmp:/tmp spred:dev9.1-tf2-py3 /usr/bin/bash $mycmd
+    -v /etc/group:/etc/group -v /tmp:/tmp thomaschoi/stk-pred:v9.1-tf2-py3 /usr/bin/bash $mycmd
+
